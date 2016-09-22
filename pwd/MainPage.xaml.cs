3da92362ac66c5c482e9a04a30ca2f872efaf85c
sai3010 <saiprasanth2007@gmail.com>
2016-09-22 22:21:14 +0530

    done logic

--- pwd/MainPage.xaml.cs
+++ pwd/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Security.Credentials;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -26,5 +28,70 @@ namespace pwd
         {
             this.InitializeComponent();
         }
+
+
+        async private void save(object sender, RoutedEventArgs e)
+        {
+            var vault = new Windows.Security.Credentials.PasswordVault();
+            vault.Add(new Windows.Security.Credentials.PasswordCredential(Resource.Text, user.Text, pwd.Password));
+            var dialog = new MessageDialog("Credentials Saved succesfully");
+            await dialog.ShowAsync();
+            Resource.Text = string.Empty;
+            user.Text = string.Empty;
+            pwd.Password = string.Empty;
+
+        }
+
+       async private void retrieve(object sender, RoutedEventArgs e)
+        {
+
+            PasswordVault vault = new PasswordVault();
+            foreach (var cred in vault.RetrieveAll())
+            {
+                cred.RetrievePassword();
+                if (cred.Resource == Resource.Text || cred.UserName == user.Text || cred.Password == pwd.Password)
+                {
+                    output.Text = "Name :" + cred.Resource + "\n" + "Password :" + cred.Password + "\n" + "UserName :" + cred.UserName;
+                }
+                else
+                {
+                    var dialog = new MessageDialog("Credentials Does Not Exist");
+                    await dialog.ShowAsync();
+                }
+                /* Console.WriteLine("Resource: {0}", cred.Resource);
+                 Console.WriteLine("UserName: {0}", cred.UserName);
+                 Console.WriteLine("Password: {0}", cred.Password);*/
+            }
+        }
+
+        async private void delete(object sender, RoutedEventArgs e)
+        {
+            PasswordVault vault = new PasswordVault();
+            foreach (var cred in vault.RetrieveAll())
+            {
+                cred.RetrievePassword();
+                if (cred.Resource == Resource.Text || cred.UserName == user.Text || cred.Password == pwd.Password)
+                {
+                    var vaul = new Windows.Security.Credentials.PasswordVault();
+                    vaul.Remove(new Windows.Security.Credentials.PasswordCredential(cred.Resource, cred.UserName, cred.Password));
+                    var dialog = new MessageDialog("Done!!!!");
+                    await dialog.ShowAsync();
+                }
+
+                else
+                {
+                    var dialog = new MessageDialog("Credentials Does Not Exist");
+                    await dialog.ShowAsync();
+                }
+            } 
+        }
     }
 }
+    
+            
+        
+    
+    
+
+   
+
